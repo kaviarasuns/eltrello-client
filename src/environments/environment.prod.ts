4f6679ea05189a408594f8817e83bc0a006d317c
Kaviarasu N S <118160152+Kaviarasu-Ceiyone@users.noreply.github.com>
2023-04-25 13:56:45 +0530

    bug test

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://34.192.190.62/api',
-  socketUrl: 'http://34.192.190.62',
+  apiUrl: 'http://34.192.190.62:80/api',
+  socketUrl: 'http://34.192.190.62:80',
 };
