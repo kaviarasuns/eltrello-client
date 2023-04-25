c32c1245e70cdacdcaf94c9caafb4bd7b3ceaefa
Kaviarasu N S <118160152+Kaviarasu-Ceiyone@users.noreply.github.com>
2023-04-25 12:54:31 +0530

    initial commit

---
+++ src/app/shared/services/boards.service.ts
@@ -0,0 +1,36 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
+import { BoardInterface } from '../types/board.interface';
+import { SocketService } from './socket.service';
+import { SocketEventsEnum } from '../types/socketEvents.enum';
+
+@Injectable()
+export class BoardsService {
+  constructor(private http: HttpClient, private socketService: SocketService) {}
+
+  getBoards(): Observable<BoardInterface[]> {
+    const url = environment.apiUrl + '/boards';
+    return this.http.get<BoardInterface[]>(url);
+  }
+
+  getBoard(boardId: string): Observable<BoardInterface> {
+    const url = `${environment.apiUrl}/boards/${boardId}`;
+    // console.log(url);
+    return this.http.get<BoardInterface>(url);
+  }
+
+  createBoard(title: string): Observable<BoardInterface> {
+    const url = environment.apiUrl + '/boards';
+    return this.http.post<BoardInterface>(url, { title });
+  }
+
+  updateBoard(boardId: string, fields: { title: string }): void {
+    this.socketService.emit(SocketEventsEnum.boardsUpdate, { boardId, fields });
+  }
+
+  deleteBoard(boardId: string): void {
+    this.socketService.emit(SocketEventsEnum.boardsDelete, { boardId });
+  }
+}
